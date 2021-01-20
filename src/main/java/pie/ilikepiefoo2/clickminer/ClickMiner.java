e3026bd37b0adbf5fd385ad5984156d1c0d5bc8a
Hunter19823 <73712365+hunter19823@users.noreply.github.com>
2021-01-20 16:49:39 -0700

    Working dev environment.

---
+++ src/main/java/pie/ilikepiefoo2/clickminer/ClickMiner.java
@@ -0,0 +1,13 @@
+package pie.ilikepiefoo2.clickminer;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("clickminer")
+public class ClickMiner {
+    private static final Logger LOGGER = LogManager.getLogger();
+    public ClickMiner(){
+        LOGGER.info("This is the start of something beautiful...");
+    }
+}
